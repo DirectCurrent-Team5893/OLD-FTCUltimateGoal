b71c19d065b51c4fa60f7c00c37b32d9c2865fc7
Yahya ElGawady <yahya.elgawady@gmail.com>
2021-01-17 12:46:10 -0600

    Construct actual Wobble class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoalComponents/WobbleArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimateGoalComponents/WobbleArm.java
@@ -1,16 +1,33 @@
 package org.firstinspires.ftc.teamcode.UltimateGoalComponents;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 import org.firstinspires.ftc.robotcontroller.internal.RobotComponent;
 import org.firstinspires.ftc.robotcontroller.internal.robotBase;
 
 public class WobbleArm extends RobotComponent {
+    public DcMotor wobbleArm;
 
+    double POWER = .5;
 
     public WobbleArm(robotBase BASE) {
         super(BASE);
+        init();
     }
-
+    void init(){
+        wobbleArm = base().getMapper().mapMotor("wobbleArm");
+    }
     public enum POSITION {OPEN_POSITION, CLOSE_POSITION}
+
+    public void runToPosition(POSITION target){
+        switch(target){
+            case OPEN_POSITION:
+                wobbleArm.setPower(POWER);
+
+            case CLOSE_POSITION:
+                wobbleArm.setPower(-POWER);
+        }
+    }
 
     @Override
     public void stop() {
